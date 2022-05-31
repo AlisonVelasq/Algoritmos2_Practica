9a2db0f3d8e8f3ab10454d29167005905ab414b0
AlisonVelasq <105014234+AlisonVelasq@users.noreply.github.com>
2022-05-31 08:32:56 -0300

    empece el ej_5

---
+++ P5-Sorting/Ej_5.cpp
@@ -0,0 +1,19 @@
+Ejercicio 5
+Se tiene un arreglo de n numeros naturales que se quiere ordenar por frecuencia, y en caso de igual frecuencia, por su valor.
+Por ejemplo, a partir del arreglo [1, 3, 1, 7, 2, 7, 1, 7, 3] se quiere obtener [1, 1, 1, 7, 7, 7, 3, 3, 2].
+Describa un algoritmo que realice el ordenamiento descrito, utilizando las estructuras de datos intermedias que considere necesarias. Calcule el orden de complejidad temporal del algoritmo propuesto.
+
+//fecuencia = cant de veces aparece el mismo elem
+//la idea es tomarlo como un multiconj, por lo cual lo guardo en un dicc
+OrdenarxFrecuencia(a:arr[], n:nat){ //longitud del array 
+    //multConj = dicc(elem:nat, cant:nat)
+    //una mejor idea es : tener las cantidades como claves para asi poder ordenar directamente
+    sinRepet <- conj(nat)
+    conjxCant <- dicc(cant:nat, elems:arr[nat])
+    //agrego los elem
+    for(i en [0, n]){
+        n = aparaciones(sinRepet, a)
+        definir(n, a[i], conjxCant)
+
+    }
+}
