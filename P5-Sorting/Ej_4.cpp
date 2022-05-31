283065161852a7f2c7246d95bc16778e0401d7a5
AlisonVelasq <105014234+AlisonVelasq@users.noreply.github.com>
2022-05-31 00:43:53 -0300

    Ej_4 falta terminar

---
+++ P5-Sorting/Ej_4.cpp
@@ -0,0 +1,4 @@
+Ejercicio 4
+Se tiene un conjunto de n secuencias {s1, s2,.., sn} en donde cada si (1 ≤ i ≤ n) es una secuencia ordenada de naturales. ¿Que metodo utilizarıa para obtener un arreglo que contenga todos los elementos de la union de los si ordenados. Describirlo. Justificar.
+
+-igual que antes, usaria un InsertionSort, por que estan parcialmente ordenadas
