e5690a8941e21dd35b34093469f86eace7bd3bd1
AlisonVelasq <105014234+AlisonVelasq@users.noreply.github.com>
2022-05-30 23:37:54 -0300

    Ej_2 terminado

---
+++ P5-Sorting/Ej_2.cpp
@@ -0,0 +1,11 @@
+Ejercicio 2
+Imagine secuencias de naturales de la forma s = Concatenar (s',s''),
+donde s' es una secuencia ordenada de froma deseada de naturales y
+s'' es una secuencia de naturales elegidos al azar.
+¿Que metodo utilizarıa para ordenar s? Justificar.
+
+ej: s'={2,4,5,6,8} s''={1,2,45,7,3,9}
+s = {2,4,5,6,8,1,2,45,7,3,9}
+rta: como la secuencia s esta parcialmente ordenada entonces eligiria InsertionSort
+ya que empezaria a "correr" mis elem cuando termine mi primer secuencia
+complejidad: O(n)
