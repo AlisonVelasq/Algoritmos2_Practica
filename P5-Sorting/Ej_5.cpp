f018f448f77226c3612fe8c1fe595e103fa21362
AlisonVelasq <105014234+AlisonVelasq@users.noreply.github.com>
2022-05-31 15:01:59 -0300

    terminar ej 5

--- P5-Sorting/Ej_5.cpp
+++ P5-Sorting/Ej_5.cpp
@@ -5,15 +5,26 @@ Describa un algoritmo que realice el ordenamiento descrito, utilizando las estru
 
 //fecuencia = cant de veces aparece el mismo elem
 //la idea es tomarlo como un multiconj, por lo cual lo guardo en un dicc
-OrdenarxFrecuencia(a:arr[], n:nat){ //longitud del array 
+OrdenarxFrecuencia(a:arr[], n:nat){ //longitud del array
     //multConj = dicc(elem:nat, cant:nat)
     //una mejor idea es : tener las cantidades como claves para asi poder ordenar directamente
     sinRepet <- conj(nat)
     conjxCant <- dicc(cant:nat, elems:arr[nat])
+    aOrdenado <- arr[]
     //agrego los elem
     for(i en [0, n]){
         n = aparaciones(sinRepet, a)
         definir(n, a[i], conjxCant)
-
+        //ordenar
+        MergeSort(conjxCant) //ordena el dicc por las claves
     }
+    itdicc = CrearIt(conjxCant)
+    //recorro las claves del dicc
+    while(siguienteClave(itdicc) != NULL){
+        while(significado(conjxCant, sig v != NULL){
+            aOrdenado.pushBack()
+
+        }
+    }
+
 }
