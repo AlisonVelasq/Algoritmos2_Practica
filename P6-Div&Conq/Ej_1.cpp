6a29a06bd21f56e90cb179318c509c53b41d6857
AlisonVelasq <105014234+AlisonVelasq@users.noreply.github.com>
2022-06-07 16:46:08 -0300

    enunciado

---
+++ P6-Div&Conq/Ej_1.cpp
@@ -0,0 +1,7 @@
+Ejercicio 1
+Escriba un algoritmo con dividir y conquistar que determine si un arreglo de tamano potencia de 2 es masa la izquierda, donde “mas a la izquierda” significa que:
+* La suma de los elementos de la mitad izquierda superan los de la mitad derecha.
+* Cada una de las mitades es a su vez “m´as a la izquierda”.
+
+Por ejemplo, el arreglo [8, 6, 7, 4, 5, 1, 3, 2] es “m´as a la izquierda”, pero [8, 4, 7, 6, 5, 1, 3, 2] no lo es.
+Intente que su solucion aproveche la tecnica de modo que complejidad del algoritmo sea estrictamente menora O(n^2).
