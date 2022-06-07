3f2e03b29eb659ddf073ec88567b05e8b858b482
AlisonVelasq <105014234+AlisonVelasq@users.noreply.github.com>
2022-06-07 19:12:27 -0300

    hice ej2, falta complejidad

--- P6-Div&Conq/Ej_2.cpp
+++ P6-Div&Conq/Ej_2.cpp
@@ -1,5 +1,21 @@
 Ejercicio 2
-Tenemos un arreglo a = [a1, a2, . . . , an] de n enteros distintos (positivos y negativos) en orden estrictamentecreciente.
+Tenemos un arreglo a = [a1, a2, . . . , an] de n enteros distintos (positivos y negativos) en orden estrictamente creciente.
 Queremos determinar si existe una posicion i tal que ai = i. Por ejemplo, dado el arreglo a =
 [−4, −1, 2, 4, 7], i = 4 es esa posicion.
 Diseñar un algoritmo dividir y conquistar eficiente (de complejidad de orden estrictamente menor que lineal) que resuelva el problema. Calcule y justifique la complejidad del algoritmo dado.
+
+//las posiciones de array se cuentan desde 1
+Pre = {estrictamente creciente}
+
+existePosicion(in A: arreglo(nat)){
+    return coincideConElIesimo(A, 0)
+}
+
+coincideConElIesimo(in A: arreglo(nat), in i:nat ){
+    if (i = tam(A)) {
+        return A[i] = i
+    }
+    else{
+        return A[i] = i && coincideConElIesimo(A, i++)
+    }
+}
