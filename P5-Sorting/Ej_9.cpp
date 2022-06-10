1a619c4aad7adcc4c5c5b821dcf5ce3bb81a2d87
AlisonVelasq <105014234+AlisonVelasq@users.noreply.github.com>
2022-06-09 21:50:51 -0300

    ARREGLOS

--- P5-Sorting/Ej_9.cpp
+++ P5-Sorting/Ej_9.cpp
@@ -1,11 +1,9 @@
-Ejercicio 9 
+Ejercicio 9
 Considere la siguiente estructura para guardar las notas de un alumno de un curso:
-alumno es tuplahnombre: string, g´enero: GEN, puntaje: Notai
+alumno es tupla(nombre: string, genero: GEN, puntaje: Notai)
 donde GEN es enum{masc, fem} y Nota es un nat no mayor que 10.
 
-Se necesita ordenar un arreglo(alumno) de forma tal que todas las mujeres aparezcan al inicio de la tabla
-seg´un un orden creciente de notas y todos los varones aparezcan al final de la tabla tambi´en ordenados de
-manera creciente respecto de su puntaje, como muestra en el siguiente ejemplo:
+Se necesita ordenar un arreglo(alumno) de forma tal que todas las mujeres aparezcan al inicio dela tabla segun un orden creciente de notas y todos los varones aparezcan al final de la tabla tambien ordenados de manera creciente respecto de su puntaje
 
 OrdenarPanilla(in planilla: arreglo(alumno)) {
 
@@ -14,7 +12,7 @@ OrdenarPanilla(in planilla: arreglo(alumno)) {
 
 arreglo(nat) res <- vacio()
 
-arreglo(lista(alumno)) bucketsF //el tamaño es 10
+arreglo(lista(alumno)) bucketsF //el tamaño DEL array es 10
 arreglo(lista(alumno)) bucketsM
 
 //agrego los buckets
@@ -22,7 +20,7 @@ for(i <- 0 to tam(planilla)-1) {
     alumno a = planilla[i]
 
     if(a.genero == "fem"){
-      AgregarAtras(bucketsf[a.puntaje], a.alumno) //agrego el alumno al bucket
+      AgregarAtras(bucketsF[a.puntaje], a.alumno) //agrego el alumno al bucket
     }
     if(a.genero == "mas"){
       AgregarAtras(bucketsM[a.puntaje], a.alumno) //agrego el alumno al bucket
@@ -34,7 +32,7 @@ for(i <- 0 to tam(planilla)-1) {
 res <- concatenar(bucketsF, bucketsM)
 return res
 
-//si es un in/out 
+//si es un in/out
 p <- concatenar(bucketsF, bucketsM)
 
 }
